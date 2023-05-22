f7b3adcf6a6bc94328d943f27edd8463ec0442eb
LarsG <lars.gunnar@jobloop.no>
2023-05-22 13:11:26 +0000

    format project

--- src/Logger/Logger.cs
+++ src/Logger/Logger.cs
@@ -1,66 +1,69 @@
 namespace Logger
 {
-  // This is a description of what methods and their signature
-  // a class using the ILogger interface is required to implement
-  interface ILogger
-  {
-    public void Info(string message);
-    public void Warn(string message);
-    public void Error(string message);
-  }
-
-  // This class **implements** the ILogger interface
-  // Notice the ":" syntaxt
-  // For this example we are logging the message to the console
-  class Console : ILogger
-  {
-    public void Info(string message)
+    // This is a description of what methods and their signature
+    // a class using the ILogger interface is required to implement
+    interface ILogger
     {
-      System.Console.WriteLine(System.String.Format( "INFO {1:hh:mm:ss}\t {0}",message, System.DateTime.Now));
-    }
-    public void Warn(string message)
-    {
-      System.Console.WriteLine(System.String.Format( "WARN {1:hh:mm:ss}\t {0}",message, System.DateTime.Now));
-    }
-    public void Error(string message)
-    {
-      System.Console.WriteLine(System.String.Format( "ERROR {1:hh:mm:ss}\t {0}",message, System.DateTime.Now));
-    }
-  }
-
-  // This class also **implements** the ILogger interface
-  // We can have multiple different implementation of the same interface
-  // which allows us to extend or chnage the functioanlity of
-  // our original program with ease
-  // For this example we are logging the message to the a file
-  class File : ILogger
-  {
-    private string logPath;
-    
-    public File(string logFilePath)
-    {
-      logPath = logFilePath;
+        public void Info(string message);
+        public void Warn(string message);
+        public void Error(string message);
     }
 
-    public void Info(string message)
+    // This class **implements** the ILogger interface
+    // Notice the ":" syntaxt
+    // For this example we are logging the message to the console
+    class Console : ILogger
     {
-      using (System.IO.StreamWriter writer = System.IO.File.AppendText(logPath))
-      {
-        writer.WriteLine(System.String.Format( "INFO {1:hh:mm:ss}\t {0}",message, System.DateTime.Now));
-      }}
+        public void Info(string message)
+        {
+            System.Console.WriteLine(System.String.Format("INFO {1:hh:mm:ss}\t {0}", message, System.DateTime.Now));
+        }
+        public void Warn(string message)
+        {
+            System.Console.WriteLine(System.String.Format("WARN {1:hh:mm:ss}\t {0}", message, System.DateTime.Now));
+        }
+        public void Error(string message)
+        {
+            System.Console.WriteLine(System.String.Format("ERROR {1:hh:mm:ss}\t {0}", message, System.DateTime.Now));
+        }
+    }
 
-    public void Warn(string message)
+    // This class also **implements** the ILogger interface
+    // We can have multiple different implementation of the same interface
+    // which allows us to extend or chnage the functioanlity of
+    // our original program with ease
+    // For this example we are logging the message to the a file
+    class File : ILogger
     {
-      using (System.IO.StreamWriter writer = System.IO.File.AppendText(logPath))
-      {
-        writer.WriteLine(System.String.Format( "WARN {1:hh:mm:ss}\t {0}",message, System.DateTime.Now));
-      }}
+        private string logPath;
 
-    public void Error(string message)
-    {
-      using (System.IO.StreamWriter writer = System.IO.File.AppendText(logPath))
-      {
-        writer.WriteLine(System.String.Format( "ERROR {1:hh:mm:ss}\t {0}",message, System.DateTime.Now));
-      }}     
-  }
+        public File(string logFilePath)
+        {
+            logPath = logFilePath;
+        }
+
+        public void Info(string message)
+        {
+            using (System.IO.StreamWriter writer = System.IO.File.AppendText(logPath))
+            {
+                writer.WriteLine(System.String.Format("INFO {1:hh:mm:ss}\t {0}", message, System.DateTime.Now));
+            }
+        }
+
+        public void Warn(string message)
+        {
+            using (System.IO.StreamWriter writer = System.IO.File.AppendText(logPath))
+            {
+                writer.WriteLine(System.String.Format("WARN {1:hh:mm:ss}\t {0}", message, System.DateTime.Now));
+            }
+        }
+
+        public void Error(string message)
+        {
+            using (System.IO.StreamWriter writer = System.IO.File.AppendText(logPath))
+            {
+                writer.WriteLine(System.String.Format("ERROR {1:hh:mm:ss}\t {0}", message, System.DateTime.Now));
+            }
+        }
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,65 +1,68 @@
 ﻿class Program
 {
-  public static void Main()
-  {
-    System.Console.WriteLine("Starting Program");
-
-    // Here we are creating an instance of our console logger
-    // Logger.ILogger log = new Logger.Console();
-
-    // Due to us using an interface and implementing
-    // different versions of the logger we can easily
-    // swap the console logger out for a file logger
-    // But we do need to decide where to store our logfiles
-    string logDirectory = "/logs";
-    string logFile = "/log.txt";
-    System.Console.WriteLine($"\nLogging to file {"." + logDirectory + logFile}\n");
-
-    Logger.ILogger log = new Logger.File("." + logDirectory + logFile);
-
-    // Place to store input string
-    string? input;
-
-    // Simple loop to query for input and logging that
-    // Needs to be forecfully shutdown
-    while (true)
+    public static void Main()
     {
-      // Logs an INFO message
-      System.Console.WriteLine("\nPlease enter a INFO message, finish with 'Enter'");
-      input = System.Console.ReadLine();
+        System.Console.WriteLine("Starting Program");
 
-      if (input == null)
-      {
-        System.Console.WriteLine("\nBlank meassages are not accepted");
-      } else
-      {
-        log.Info(input);
-      }
+        // Here we are creating an instance of our console logger
+        // Logger.ILogger log = new Logger.Console();
 
-      // Logs an WARN message
-      System.Console.WriteLine("\nPlease enter a WARN message, finish with 'Enter'");
-      input = System.Console.ReadLine();
+        // Due to us using an interface and implementing
+        // different versions of the logger we can easily
+        // swap the console logger out for a file logger
+        // But we do need to decide where to store our logfiles
+        string logDirectory = "/logs";
+        string logFile = "/log.txt";
+        System.Console.WriteLine($"\nLogging to file {"." + logDirectory + logFile}\n");
 
-      if (input == null)
-      {
-        System.Console.WriteLine("\nBlank meassages are not accepted");
-      } else
-      {
-        log.Warn(input);
-      }
+        Logger.ILogger log = new Logger.File("." + logDirectory + logFile);
 
-      // Logs an ERROR message
-      System.Console.WriteLine("\nPlease enter a ERROR message, finish with 'Enter'");
-      input = System.Console.ReadLine();
+        // Place to store input string
+        string? input;
 
-      if (input == null)
-      {
-        System.Console.WriteLine("\nBlank meassages are not accepted");
-      } else
-      {
-        log.Error(input);
-      }
+        // Simple loop to query for input and logging that
+        // Needs to be forecfully shutdown
+        while (true)
+        {
+            // Logs an INFO message
+            System.Console.WriteLine("\nPlease enter a INFO message, finish with 'Enter'");
+            input = System.Console.ReadLine();
+
+            if (input == null)
+            {
+                System.Console.WriteLine("\nBlank meassages are not accepted");
+            }
+            else
+            {
+                log.Info(input);
+            }
+
+            // Logs an WARN message
+            System.Console.WriteLine("\nPlease enter a WARN message, finish with 'Enter'");
+            input = System.Console.ReadLine();
+
+            if (input == null)
+            {
+                System.Console.WriteLine("\nBlank meassages are not accepted");
+            }
+            else
+            {
+                log.Warn(input);
+            }
+
+            // Logs an ERROR message
+            System.Console.WriteLine("\nPlease enter a ERROR message, finish with 'Enter'");
+            input = System.Console.ReadLine();
+
+            if (input == null)
+            {
+                System.Console.WriteLine("\nBlank meassages are not accepted");
+            }
+            else
+            {
+                log.Error(input);
+            }
+        }
+
     }
-
-  }
 }
